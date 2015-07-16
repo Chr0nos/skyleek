29fdc9f754a654a2c6f8b4475accd4bd14cb57d6
Ludwig DUBOS <jalibter@gmail.com>
2015-07-16 22:31:15 +0200

    Adding some includes in main.js

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
+include('ft_array');
+include('ft_cell');
+include('ft_chips');
+include('ft_effect_count');
+include('ft_heal');
+include('ft_leeks');
+include('ft_move');
+include('ft_weapon');
+
 function main()
 {
 
 }
+
 main();
