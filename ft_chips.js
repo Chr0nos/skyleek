f8071bcc05e467eda86b47e456aeb7624e1e2d10
Chr0nos <hisoka2501@free.fr>
2015-07-29 02:55:27 +0000

    added many function in ft_chips

--- ft_chips.js
+++ ft_chips.js
@@ -1,4 +1,6 @@
 include("ft_effect_count");
+include("IA_Bubbel");
+include("ft_array");
 
 function ft_chips_getChipByEffect(effect)
 {
@@ -25,3 +27,84 @@ function ft_chips_getChipByEffect(effect)
 	}
 	return effect_chips;
 }
+
+function ft_chip_is_summon(chip)
+{
+	var chips = [];
+	
+	push(chips, CHIP_PUNY_BULB);
+	push(chips, CHIP_FIRE_BULB);
+	push(chips, CHIP_ICED_BULB);
+	return ft_inArray(chips, chip, count(chips));
+}
+
+function ft_chip_summon(chip)
+{
+	var cell;
+
+	cell = getCell() + 1;
+	while ((getCellContent(cell) != CELL_EMPTY) && (cell < 613))
+	{
+		cell++;
+	}
+	summon(CHIP_PUNY_BULB, cell, bubble_main);
+}
+
+function ft_chips(enemy)
+{
+	var chips = [];
+	var chip;
+	var n;
+
+	push(chips, CHIP_SPARK);
+	//push(chips, CHIP_ROCK);
+	push(chips, CHIP_FLAME);
+	push(chips, CHIP_LIGHTNING);
+	push(chips, CHIP_PUNY_BULB);
+	n = count(chips);
+	while (n--)
+	{
+		chip = chips[n];
+		if (ft_chip_is_summon(chip))
+		{
+			ft_chip_summon(chip);
+		}
+		else if (canUseChip(chip, enemy))
+		{
+			useChip(chip, enemy);
+		}
+	}
+}
+
+function ft_chip_zone(chip, leek)
+{
+	var dcells = [];
+	//todo
+	return dcells;
+}
+
+function ft_chip_aera(chip, leek, cell)
+{
+	var cells = [];
+	var ccell;
+	
+	ccell = 613;
+	while (ccell--)
+	{
+		//todo
+	}
+}
+
+function ft_diff(@tab1, @tab2)
+{
+	var result = [];
+	var n;
+
+	n = count(tab1);
+	while (n--)
+	{
+		if (!inArray(tab2, tab1[n])) push(result, tab1[n]);
+	}
+	return result;
+}
+
